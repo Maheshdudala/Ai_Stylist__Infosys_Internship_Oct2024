94a51b174c7c3006fa814fc3564eabbde3ef1f14
singhrahul03 <rahul86197@outlook.com>
2024-10-25 18:22:35 +0530

    modify data split

--- Data Split/split.py
+++ Data Split/split.py
@@ -2,7 +2,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('data.csv')
-X = df.drop(columns=['target_column'])
-y = df['target_column']
+X = df.drop(columns=['link'])
+y = df['link'] 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print(f"Training set size: {X_train.shape[0]} rows")
+print(f"Test set size: {X_test.shape[0]} rows")
